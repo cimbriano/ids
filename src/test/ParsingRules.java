8b5fac5274c5200a77472e6c8984a4a05bb0b05e
Dustin <dzelle@seas.upenn.edu>
2012-03-08 17:35:25 -0500

    Fleshed out test--it parses and rebuilds all rules now.

--- src/test/ParsingRules.java
+++ src/test/ParsingRules.java
@@ -1,70 +1,19 @@
 import java.io.*;
+import parse.*;
 import def.*;
 
 public class ParsingRules {
 
     static public void main(String argv[]) {
        	try {
-	    System.out.print("Parsing (" + argv[0] + ")... ");
 
 	    parser p = new parser(new lexer(new FileReader(argv[0])));
 	    Object d = p.parse().value;
 
 	    if (d instanceof ThreatDefinition) {
-		System.out.println("done.");
 		((ThreatDefinition) d).print();
-	    } else
-		System.out.println("failed.");
-	    
-	    /*lexer l = new lexer(new FileReader(argv[0]));
-	    java_cup.runtime.Symbol s = l.next_token();
-
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    s = l.next_token();
-	    System.out.println(s.toString());
-	    System.out.println(sym.SET_HOST);*/
+		System.out.print("\n\n");
+	    }
 
 	} catch (Exception e) {
 	    e.printStackTrace();
--- src/test/run.sh
+++ src/test/run.sh
@@ -2,5 +2,6 @@
 
 for f in `ls ../../etc/rules`
 do
+    echo "*** $f ***"
     java -classpath ../../lib:../../bin:. ParsingRules "../../etc/rules/$f"
 done
