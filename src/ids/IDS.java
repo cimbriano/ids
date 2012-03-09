9ae345ae56bd7783d7ffe95d1b537b693b591b26
Dustin <dzelle@seas.upenn.edu>
2012-03-09 11:26:52 -0500

    Added parsering to IDS's main.

--- src/ids/IDS.java
+++ src/ids/IDS.java
@@ -3,28 +3,42 @@ package ids;
 import java.util.*;
 import java.io.*;
 import parse.*;
+import def.*;
 
 public class IDS
 {
     static final String usage = "Usage: IDS [rule_file] [pcap_file]";
+
     /**
      * @param args
      */
     public static void main(String[] argv) {
+	if (argv.length < 2) die(usage);	
 
-	if (argv.length < 2) {
-	    System.err.println(usage);
-	    System.exit(1);
-	}
+	ThreatDefinition d = loadDefinition(argv[0]);
+    }
 
-	System.out.println("Hello!");
-	System.exit(0);
-
+    private static ThreatDefinition loadDefinition(String fname) {
 	try {
-	    parser p = new parser(new lexer(new FileReader(argv[0])));
-	    Object d = p.parse().value;    
+	    parser p = new parser(new lexer(new FileReader(fname)));
+	    Object d = p.parse().value;
+
+	    if (d instanceof ThreatDefinition)
+		return (ThreatDefinition) d;
+	} catch (FileNotFoundException e) {
+	    die("File not found: "+fname);
 	} catch (Exception e) {
+	    die("Parser exception: "+e.getMessage());
 	}
+
+	die("Error loading ThreatDefinition: "+fname);
+
+	return null;
+    }
+
+    private static void die(String msg) {
+	System.err.println(msg);
+	System.exit(1);
     }
 
 }
