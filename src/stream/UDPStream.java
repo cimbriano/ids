e19db1c28cbcbebd7031936243aedee974a8968a
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-03-10 14:02:32 -0500

    UDPStreamIterator inner class skeleton

--- src/stream/UDPStream.java
+++ src/stream/UDPStream.java
@@ -20,12 +20,39 @@ public class UDPStream implements AbstractStream {
 	@Override
 	public Iterator<IPPacket> iterator() {
 		// TODO Auto-generated method stub
-		return null;
+		return new UDPStreamIterator(this);
 	}
 
 	@Override
 	public void add(IPPacket packet) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	private class UDPStreamIterator implements Iterator<IPPacket>{
+
+
+		public UDPStreamIterator(UDPStream udpStream) {
+			//TODO implement this 
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public IPPacket next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 
