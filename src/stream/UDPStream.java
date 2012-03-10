32d1bd6fcb758f30e4e9dd916f124a1ef72d5cfb
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-03-10 14:20:51 -0500

    Removed internal iterator class in place of using the LnikedList's iterator

--- src/stream/UDPStream.java
+++ src/stream/UDPStream.java
@@ -1,30 +1,37 @@
 package stream;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 import net.sourceforge.jpcap.net.IPPacket;
 
 public class UDPStream implements AbstractStream {
+	LinkedList<IPPacket> packetList;
+	StreamKey streamKey;
+	
+	public UDPStream(StreamKey key){
+		packetList = new LinkedList<IPPacket>();
+		streamKey = key;
+	}
+	
 
-    public StreamKey getKey() {
-	return null;
-    }
+  public StreamKey getKey() {
+			return streamKey;
+  }
 
 	@Override
 	public boolean isTCP() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isUDP() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public Iterator<IPPacket> iterator() {
 		// TODO Auto-generated method stub
-		return new UDPStreamIterator(this);
+		return packetList.iterator();
 	}
 
 	@Override
@@ -33,31 +40,5 @@ public class UDPStream implements AbstractStream {
 		
 	}
 	
-	private class UDPStreamIterator implements Iterator<IPPacket>{
-
-
-		public UDPStreamIterator(UDPStream udpStream) {
-			//TODO implement this 
-		}
-
-		@Override
-		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public IPPacket next() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public void remove() {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-
+	
 }
