ca4fbd0b5b877bb5b23985759d5182d0785f6530
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-03-10 13:21:34 -0500

    Added some imports and new ids package files to its Makefile

--- src/ids/AbstractStream.java
+++ src/ids/AbstractStream.java
@@ -1,8 +1,9 @@
 package ids;
 
-import java.util.Iterator;
+import net.sourceforge.jpcap.net.IPPacket;
 
 public interface AbstractStream extends Iterable<IPPacket> {
 	public boolean isTCP();
 	public boolean isUDP();
+	public void add(IPPacket packet);
 }
--- src/ids/IDSListener.java
+++ src/ids/IDSListener.java
@@ -1,8 +1,13 @@
 package ids;
 
+import net.sourceforge.jpcap.capture.PacketListener;
+import net.sourceforge.jpcap.net.Packet;
+
 public class IDSListener implements PacketListener{
-	
-	public void packetArrvied(Packet p){
+
+	@Override
+	public void packetArrived(Packet packet) {
+		// TODO Auto-generated method stub
 		
 	}
 }
