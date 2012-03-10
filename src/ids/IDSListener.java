f384cf1ce3b723bf5a77a838b7283c76ce341715
Dustin <dzelle@seas.upenn.edu>
2012-03-10 13:39:06 -0500

    Refactor.

--- src/ids/IDSListener.java
+++ src/ids/IDSListener.java
@@ -3,9 +3,8 @@ package ids;
 import net.sourceforge.jpcap.capture.PacketListener;
 import net.sourceforge.jpcap.net.Packet;
 
-public class IDSListener implements PacketListener{
+public class IDSListener implements PacketListener {
 
-	@Override
 	public void packetArrived(Packet packet) {
 		// TODO Auto-generated method stub
 		
