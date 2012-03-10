053b01be2e1138846260425e44dd326c685f2436
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-03-10 13:58:55 -0500

    -XLint:unchecked flag to Makefile for warnings, TCPStreamIterator inner class skeleton

--- src/stream/TCPStream.java
+++ src/stream/TCPStream.java
@@ -20,12 +20,38 @@ public class TCPStream implements AbstractStream {
 	@Override
 	public Iterator<IPPacket> iterator() {
 		// TODO Auto-generated method stub
-		return null;
+		return new TCPStreamIterator(this);
 	}
 
 	@Override
 	public void add(IPPacket packet) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	private class TCPStreamIterator implements Iterator<IPPacket> {
+	
+		public TCPStreamIterator(TCPStream tcpStream){
+			//TODO this should take a parameter
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public IPPacket next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 	
