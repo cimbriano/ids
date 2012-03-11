9f624e55258bd6d6d163bee81e40d6761622f21e
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-03-10 20:52:46 -0500

    Added abstract class for data strucutre to keep track of sub rule matching, as well as TCP and UDP instantiations of it

---
+++ src/def/UDPProtocolRuleChecker.java
@@ -0,0 +1,13 @@
+package def;
+
+import net.sourceforge.jpcap.net.UDPPacket;
+
+public class UDPProtocolRuleChecker implements AbstractProtocolRuleChecker<UDPPacket> {
+
+	@Override
+	public void add(UDPPacket packet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
