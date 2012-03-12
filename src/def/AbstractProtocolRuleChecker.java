773fa985987db22236a07635598abd4cbe2aa488
Dustin <dzelle@seas.upenn.edu>
2012-03-12 19:26:42 -0400

    Finished (almost) TCPStreamRule.java

--- src/def/AbstractProtocolRuleChecker.java
+++ src/def/AbstractProtocolRuleChecker.java
@@ -3,18 +3,25 @@ package def;
 import net.sourceforge.jpcap.net.*;
 import java.util.regex.*;
 import java.util.List;
+import java.io.*;
 
 public abstract class AbstractProtocolRuleChecker<T extends IPPacket, S extends AbstractRule> {
 
     public abstract void add(T packet, S rule, String rulename, String host);
 
     protected boolean checkContents(IPPacket packet, ProtocolSubrule subRule) {
-	String data = new String(packet.getData());
+	try {
+	    String data = new String(packet.getData(), "ISO-8859-1");
 
-	Pattern p = Pattern.compile(subRule.regexp);
-	Matcher m = p.matcher(data);
+	    Pattern p = Pattern.compile(subRule.regexp);
+	    Matcher m = p.matcher(data);
 
-	return m.find();
+	    return m.find();
+	} catch (UnsupportedEncodingException e) {
+	    e.printStackTrace();
+	}
+
+	return false;
     }
 
   protected void alert(String rulename, List<T> matchedPackets){
